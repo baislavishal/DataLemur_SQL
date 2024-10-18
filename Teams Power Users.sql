d06599702c00fa28fa55a061b1b391a40aa38118
Vishal Baisla <33949655+baislavishal@users.noreply.github.com>
2024-10-18 08:36:46 +0530

    Add files via upload

---
+++ Teams Power Users.sql
@@ -0,0 +1,6 @@
+#Teams Power Users - Microsoft
+
+SELECT sender_id,COUNT(message_id) AS message_count 
+  FROM messages 
+  WHERE sent_date BETWEEN '08-01-2022' AND '08-31-2022'
+  GROUP BY sender_id ORDER BY message_count DESC LIMIT 2;
